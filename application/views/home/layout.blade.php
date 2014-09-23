075ecb377361886e2bf7aaa2a8349c350530e289
Dileep D V <dileep.dv@costprize.com>
2014-09-23 17:53:35 +0530

    modified index..blade and layout.blade

--- application/views/home/layout.blade.php
+++ application/views/home/layout.blade.php
@@ -51,7 +51,7 @@ margin-left: 180px;
 @_yield('content')
 <div class="footer">
     <p>&copy; Copyright 2014. Designed by <a target="_blank"
-                                             href="http://www.Costprize.com">Costprize.com</a>
+                                             href="http://www.costprize.com">Costprize.com</a>
     </p>
     <ul>
         <li style="border-left: medium none;"><a href="{{ URL::to('http://localhost/costprize_blog/public/') }}"><span>Home</span></a></li>
